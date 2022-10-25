4efe354545425cae3354f8c3eeeaf940512abcfc
bijubayarea <bijubayarea@gmail.com>
2022-10-25 10:35:45 -0700

    Add non-owner S3 bucket to test

--- src/irsa.tf
+++ src/irsa.tf
@@ -14,8 +14,8 @@ data "aws_iam_policy_document" "s3_policy" {
     ]
 
     resources = [
-      aws_s3_bucket.irsa.arn,
-      "${aws_s3_bucket.irsa.arn}/*"
+      aws_s3_bucket.irsa_owner.arn,
+      "${aws_s3_bucket.irsa_owner.arn}/*"
     ]
   }
 }
--- src/s3.tf
+++ src/s3.tf
@@ -1,14 +1,29 @@
-resource "aws_s3_bucket" "irsa" {
-  bucket        = "bijubayarea-s3-test"
+resource "aws_s3_bucket" "irsa_owner" {
+  bucket        = "bijubayarea-s3-test-owner"
   force_destroy = true
 
   tags = {
-    Name = "s3 irsa test bucket"
+    Name = "s3 irsa test bucket owner"
     env  = "test"
   }
 }
 
-resource "aws_s3_bucket_acl" "irsa_bucket_acl" {
-  bucket = aws_s3_bucket.irsa.id
+resource "aws_s3_bucket_acl" "irsa_bucket_owner_acl" {
+  bucket = aws_s3_bucket.irsa_owner.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "irsa_non_owner" {
+  bucket        = "bijubayarea-s3-test-non-owner"
+  force_destroy = true
+
+  tags = {
+    Name = "s3 irsa test bucket non-owner"
+    env  = "test"
+  }
+}
+
+resource "aws_s3_bucket_acl" "irsa_bucket_non_owner_acl" {
+  bucket = aws_s3_bucket.irsa_non_owner.id
   acl    = "private"
 }
